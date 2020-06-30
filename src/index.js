f3bef59b04e1e8f6716d713436cd2feb11406228
Mohamed Schuster <mohamedschdev@gmail.com>
2020-06-30 15:24:30 -0300

    sequelize configured.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 const express = require('express');
 
+const db = require('./models')
+
 const authControllers = require('./controllers/auth');
 
 const app = express();
 
 app.use('/auth', authControllers);
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
   return res.json('API running...')
 });
 
-app.listen(3001, () => {
-  console.log('Listening on port 3001...');
+db.sequelize.sync().then(() => {
+  app.listen(3001, () => {
+    console.log('Listening on port 3001...');
+  });
 });
