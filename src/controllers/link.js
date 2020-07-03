800cf3a647c3265dde7efa1fd2145e9718fd84c6
Mohamed Schuster <mohamedschdev@gmail.com>
2020-07-03 16:36:55 -0300

    Excluded auth paths from JWT.

--- src/controllers/link.js
+++ src/controllers/link.js
@@ -4,42 +4,42 @@ const { Link } = require('../models')
 
 const router = express.Router();
 
-router.get('/' , async (req, res) => {
-  const {accountId} = req;
-  const link = await Link.findAll({where: {accountId} });
+router.get('/', async (req, res) => {
+  const { accountId } = req;
+  const link = await Link.findAll({ where: { accountId } });
   return res.jsonOK(link)
 });
 
-router.get('/:id' , async (req,res) => {
-  const {accountId} = req;
-  const {id} = req.params;
-  const link = await Link.findOne({where: {id,accountId} });
-  if(!link) return res.jsonNotFound();
+router.get('/:id', async (req, res) => {
+  const { accountId } = req;
+  const { id } = req.params;
+  const link = await Link.findOne({ where: { id, accountId } });
+  if (!link) return res.jsonNotFound();
   return res.jsonOK(link)
 });
 
-router.post('/' , async (req, res) => {
-  const {accountId , body} = req;
-  const {label,url,isSocial} = body;
+router.post('/', async (req, res) => {
+  const { accountId, body } = req;
+  const { label, url, isSocial } = body;
 
   const image = 'http://google.com/image.jpg';
-  const link = await Link.create({label,url,isSocial,image,accountId});
+  const link = await Link.create({ label, url, isSocial, image, accountId });
 
   return res.jsonOK(link)
 });
 
-router.put('/:id' , async (req,res) => {
-  const {accountId , body} = req;
-  const {id} = req.params;
-  const fields = ['label', 'url' , 'isSocial'];
+router.put('/:id', async (req, res) => {
+  const { accountId, body } = req;
+  const { id } = req.params;
+  const fields = ['label', 'url', 'isSocial'];
 
-  const link = await Link.findOne({where: {id,accountId} });
-  
-  if(!link) return res.jsonNotFound();
+  const link = await Link.findOne({ where: { id, accountId } });
 
-  fields.map((fieldName)=>{
+  if (!link) return res.jsonNotFound();
+
+  fields.map((fieldName) => {
     const newValue = body[fieldName];
-    if(newValue) link[fieldName] = newValue;
+    if (newValue) link[fieldName] = newValue;
   });
 
   await link.save();
@@ -49,12 +49,12 @@ router.put('/:id' , async (req,res) => {
 });
 
 
-router.delete('/:id' , async (req,res) => {
-  const {accountId} = req;
-  const {id} = req.params;
-  const link = await Link.findOne({where: {id,accountId} });
-  if(!link) return res.jsonNotFound();
-  
+router.delete('/:id', async (req, res) => {
+  const { accountId } = req;
+  const { id } = req.params;
+  const link = await Link.findOne({ where: { id, accountId } });
+  if (!link) return res.jsonNotFound();
+
   await link.destroy();
   return res.jsonOK();
 
--- src/middlewares/jwt.js
+++ src/middlewares/jwt.js
@@ -1,20 +1,30 @@
-const {verifyJwt} = require('../helpers/jwt');
+const { verifyJwt } = require('../helpers/jwt');
 
-const checkJwt = (req,res,next) => {
+const checkJwt = (req, res, next) => {
+
+  const { url: path } = req;
+
+  const excludedPaths = ['/auth/sign-in', '/auth/sign-up'];
+
+  const isExcluded = !!excludedPaths.find((p) => p.startsWith(path));
+  if (isExcluded) return next()
+  
+  console.log(path , isExcluded);
+  
   let token = req.headers['authorization'];
-  token = token ? token.slice(7,token.length) : null;
-  if(!token) {
+  token = token ? token.slice(7, token.length) : null;
+  if (!token) {
     return res.jsonUnauthorized(null, 'invalid token');
   };
   try {
     const decoded = verifyJwt(token);
     req.accountId = decoded.id;
-  next();
+    next();
   } catch (error) {
     return res.jsonUnauthorized(null, 'invalid token');
   }
 
-  
+
 
 };
 
